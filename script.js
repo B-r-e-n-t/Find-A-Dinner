ebf87ffcff3da892fd0c6f281b80dfb8a6ef0c96
Brent <jbmsharpe@gmail.com>
2022-12-15 10:49:20 -0500

    Now using spoonacular API and now you go to the recipe link in a new window when you click on the image.

--- script.js
+++ script.js
@@ -1,114 +1,44 @@
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
-const random = document.getElementById('random');
 const mealsEl = document.getElementById('meals');
 const resultHeading = document.getElementById('result-heading');
-const single_mealEl = document.getElementById('single-meal');
 
 
 function searchMeal(e) {
     e.preventDefault();
 
-    single_mealEl.innerHTML = '';
-
     const term = search.value;
 
     if(term.trim()) {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+        fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${term}&number=100&addRecipeInformation=true&apiKey=1c7cee04f89940d3b7996e14eafd77a0`)
+        
         .then(res => res.json())
         .then(data => {
             console.log(data);
             resultHeading.innerHTML = `<h2>Search results for '${term}':<h2>`;
 
-            if(data.meals === null) {
-                resultHeading.innerHTML = `<p>No meals found, Try again!<p>`
+            if(data.results.length === 0) {
+                resultHeading.innerHTML = `<p>No meals found, try again! <br> Be sure to check your spelling.<p>`
             } else {
-                mealsEl.innerHTML = data.meals.map(meal =>
-                `<div class="meal">
-                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                    <div class="meal-info" data-mealID="${meal.idMeal}">
-                    <h3>${meal.strMeal}</h3>
+                mealsEl.innerHTML = data.results.map(meal =>
+                `<a href='${meal.sourceUrl}' class='recipe-link' target='_blank'>
+                    <div class="meal">
+                        <img src="${meal.image}" alt="${meal.title}" />
+                        <div class="meal-info" data-mealID="${meal.id}">
+                        <h3>${meal.title}</h3>
+                        </div>
+                      
                     </div>
-                </div>`)
+                </a>`)
                 .join('');
             }
         });
         search.value = '';
     } else {
-        alert('Please enter a search term')
+        resultHeading.innerHTML = `<p>Please enter a search term<p>`
     }
-}
-
-//fetch meal by ID
-function getMealById(mealID) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-    .then(res => res.json())
-    .then(data => {
-        const meal = data.meals[0];
-
-        addMealToDOM(meal);
-    })
-}
-
-function getRandomMeal() {
-    mealsEl.innerHTML = '';
-    resultHeading.innerHTML = '';
-
-    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-    .then(res => res.json())
-    .then(data => {
-        const meal = data.meals[0];
-
-        addMealToDOM(meal);
-    });
-}
-
-function addMealToDOM(meal) {
-    const ingredients = [];
-
-    for(let i = 1; i <= 20; i++) {
-        if(meal[`strIngredient${i}`]) {
-            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
-        }else {
-            break;
-        }
-    }
-
-    single_mealEl.innerHTML = `
-    <div class="single-meal">
-        <h1>${meal.strMeal}</h1>
-        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-        <div class="single-meal-info">
-         ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
-         ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}   
-        </div>
-        <div class="main">
-            <p>${meal.strInstructions}</p>
-            <h2>Ingredients</h2>
-            <ul>
-            ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
-            </ul>
-        </div>
-    </div>
-    `;
 }
 
 submit.addEventListener('submit', searchMeal);
-random.addEventListener('click', getRandomMeal);
-
-mealsEl.addEventListener('click', e => {
-    const mealInfo = e.path.find(item => {
-        if (item.classList) {
-            return item.classList.contains('meal-info');
-        } else {
-            return false
-        }
-    });
-
-    if(mealInfo) {
-        const mealID = mealInfo.getAttribute('data-mealid');
-        getMealById(mealID);
-    }
-});
 
 
