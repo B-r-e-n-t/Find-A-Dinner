0b0067dfea31e91a32911adfe5c4090c9b0c5ae3
Brent <jbmsharpe@gmail.com>
2026-03-05 08:53:24 -0500

    changed to a different file, copying code and pasting it

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 
+console.log("script.js loaded");
 
+function toggleMenu() {
+    console.log("Hamburger clicked");
+    const menu = document.getElementById("mobileMenu");
+
+    if (!menu) {
+        console.log("Mobile menu element NOT FOUND");
+        return;
+    }
+
+    menu.classList.toggle("show");
+}
+
+
+
+
+
+
+function scrollToContact() {
+  const el = document.getElementById("contact");
+  if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+function scrollToSection(id) {
+  const el = document.getElementById(id);
+  if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+  }
+}
 
 
 
